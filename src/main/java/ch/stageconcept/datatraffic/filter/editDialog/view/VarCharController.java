49df1cb5a108ba294570d10abafae8674124999d
oldu73 <oldu73@gmail.com>
2016-06-14 23:13:26 +0200

    Code cleaning 10

--- src/main/java/ch/stageconcept/datatraffic/filter/editDialog/view/VarCharController.java
+++ src/main/java/ch/stageconcept/datatraffic/filter/editDialog/view/VarCharController.java
@@ -1,16 +1,13 @@
 package ch.stageconcept.datatraffic.filter.editDialog.view;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 
 public class VarCharController extends TypeController<String> {
 
 	@FXML
 	public void initialize() {
 
+		/*
 		filterValue.textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -28,6 +25,22 @@ public class VarCharController extends TypeController<String> {
 				}
 			}
 		});
+		 */
+
+		filterValue.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.isEmpty()) {
+                filterValue.setText("");
+                value = "";
+                valueEmpty.set(true);
+            } else {
+                try {
+                    value = newValue;
+                    valueEmpty.set(false);
+                } catch (NumberFormatException e) {
+                    filterValue.setText(oldValue);
+                }
+            }
+        });
 
 	}
 
