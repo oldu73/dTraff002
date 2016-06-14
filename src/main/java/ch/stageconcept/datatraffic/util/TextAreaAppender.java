abca696adfc9de5b8bbcf3af553b37a2e98c7427
oldu73 <oldu73@gmail.com>
2016-06-14 23:46:58 +0200

    Code cleaning 19

--- src/main/java/ch/stageconcept/datatraffic/util/TextAreaAppender.java
+++ src/main/java/ch/stageconcept/datatraffic/util/TextAreaAppender.java
@@ -15,7 +15,6 @@ public class TextAreaAppender extends WriterAppender {
     /**
      * Set the target TextArea for the logging information to appear.
      *
-     * @param textArea
      */
     public static void setTextArea(final TextArea textArea) {
         TextAreaAppender.textArea = textArea;
@@ -24,7 +23,6 @@ public class TextAreaAppender extends WriterAppender {
     /**
      * Format and then append the loggingEvent to the stored TextArea.
      *
-     * @param loggingEvent
      */
     @Override
     public void append(final LoggingEvent loggingEvent) {
@@ -32,6 +30,8 @@ public class TextAreaAppender extends WriterAppender {
 
         // Append formatted message to text area using the Thread.
         try {
+
+            /*
             Platform.runLater(new Runnable() {
                 @Override
                 public void run() {
@@ -51,6 +51,25 @@ public class TextAreaAppender extends WriterAppender {
                     }
                 }
             });
+             */
+
+            Platform.runLater(() -> {
+                try {
+                    if (textArea != null) {
+                        if (textArea.getText().length() == 0) {
+                            textArea.setText(message);
+                        } else {
+                            textArea.selectEnd();
+                            textArea.insertText(textArea.getText().length(),
+                                    message);
+                        }
+                    }
+                } catch (final Throwable t) {
+                    System.out.println("Unable to append log to text area: "
+                            + t.getMessage());
+                }
+            });
+
         } catch (final IllegalStateException e) {
             // ignore case when the platform hasn't yet been iniitialized
         }
